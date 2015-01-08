9a183a7b6a2d3abda2884dd0f0560eb2ac15d101
h0lybyte <admin@kbve.com>
2015-01-08 11:30:25 -0500

    Update iptable_forward.sh

--- iptable_forward.sh
+++ iptable_forward.sh
@@ -1,3 +1,3 @@
-iptables -t nat -A PREROUTING -p tcp -d 142.4.212.92 --dport $1  -i eth0 -j DNAT --to-destination $2
+iptables -t nat -A PREROUTING -p tcp -d $1 --dport $2  -i eth0 -j DNAT --to-destination $3
 #chmod +x iptable_forward.sh 
-# ./iptable_forward.sh <port number> <bind ip>
+# ./iptable_forward.sh <public ip> <port number> <bind ip>
